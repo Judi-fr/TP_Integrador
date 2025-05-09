597af6d4155c705c62dab701fdc540646e40fb95
Judi-fr <juanitoalcachofa55@hotmail.com>
2025-05-09 02:14:47 -0300

    me olvide de cambiar el nombre de los archivos que se van a modificar cada vez que se haga un append

--- 2_metodosArchivos.cpp
+++ 2_metodosArchivos.cpp
@@ -7,16 +7,31 @@ Archivos::Archivos(std::string nombre){
 
 ///APPEND///////////////////////////////////////
 void Archivos::append(Suscripcion suscripcion){
-    FILE *pArchivo = fopen(_nombreArchivo.c_str(),"ab");
+    FILE *pArchivo = fopen("Suscripciones.dat","ab");
     if(pArchivo==NULL){
         return;
     }
+    fwrite(&suscripcion,sizeof(Suscripcion),1,pArchivo);
+    fclose(pArchivo);
+    return;
 }
 void Archivos::append(Cancion cancion){
-
+    FILE *pArchivo = fopen("Canciones.dat","ab");
+    if(pArchivo==NULL){
+        return;
+    }
+    fwrite(&cancion,sizeof(Cancion),1,pArchivo);
+    fclose(pArchivo);
+    return;
 }
 void Archivos::append(Accesos acceso){
-
+    FILE *pArchivo = fopen("Accesos.dat","ab");
+    if(pArchivo==NULL){
+        return;
+    }
+    fwrite(&acceso,sizeof(Accesos),1,pArchivo);
+    fclose(pArchivo);
+    return;
 }
 
 /////////////////////////////////////////////////
